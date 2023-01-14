13a5bb91d073739a5609199a625d4779bb9d2fbc
bhai <svchk@gmail.com>
2023-01-14 17:10:57 +0530

    Updated migration

--- database/migrations/2023_01_14_092811_create_files_table.php
+++ database/migrations/2023_01_14_092811_create_files_table.php
@@ -12,11 +12,7 @@ return new class extends Migration
      * @return void
      */
     public function up()
-    {
-        /*Schema::create('files', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });*/
+    { 
         Schema::create('files', function (Blueprint $table) {
             $table->id();
             $table->string('name');
